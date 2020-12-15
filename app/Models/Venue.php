1dfddde6fb0b0b698f0c41b9d6a61e7cf1ac6e10
Nikolaos Christos Antoniadis <nikosantoniadis94@gmail.com>
2020-12-15 17:06:54 +0200

    Models/Controllers/Migration Creation
    + City mcm (= Model / Controller / Migration)
    + Event mcm
    + Venue mcm

---
+++ app/Models/Venue.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Venue extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'city_id',
+        'postal_code',
+        'phone',
+        'image_url'
+    ];
+
+}
