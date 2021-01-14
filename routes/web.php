3e894cd054d3d99e9d32b1ffe2092a3d7ca847cc
Nikolaos Christos Antoniadis <nikosantoniadis94@gmail.com>
2021-01-14 15:50:33 +0200

    Added Routes
    Added Routes for most of the common actions for the app

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\VenueController;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,15 +18,38 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EventController::class, 'index'])->name('event_index');
+//Route::get('/event/show/{id}', [EventController::class, 'show'])->name('event_checkin');
 
 Auth::routes();
 
 Route::middleware('auth')->group(static function () {
-    Route::get('/cart', [''])->name('cart');
-});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    /**
+     * Home test route.
+     */
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    /**
+     * Reservation routes.
+     */
+    Route::get('/reservations', [ReservationController::class, 'index'])->name('reservation_index');
+    Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservation_create');
+    Route::get('/reservations/{id}/edit', [ReservationController::class, 'edit'])->name('reservation_edit');
+    Route::get('/reservations/{id}', [ReservationController::class, 'show'])->name('reservation_show');
+
+    /**
+     * Event routes.
+     */
+    Route::get('/event/create', [EventController::class, 'create'])->name('event_create');
+    Route::get('/event/{id}/edit', [EventController::class, 'edit'])->name('event_edit');
+    Route::get('/event/{id}', [EventController::class, 'show'])->name('event_show');
+
+    /**
+     * Venue routes.
+     */
+    Route::get('/venue', [VenueController::class, 'index'])->name('venue_index');
+    Route::get('/venue/create', [VenueController::class, 'create'])->name('venue_create');
+    Route::get('/venue/{id}/edit', [VenueController::class, 'edit'])->name('venue_edit');
+    Route::get('/venue/{id}', [VenueController::class, 'show'])->name('venue_show');
+});
