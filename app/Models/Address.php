ab925e031dc5ccded4e4466ad5bf3a88ef890f01
Nikolaos Christos Antoniadis <nikosantoniadis94@gmail.com>
2021-02-01 23:36:59 +0200

    Adjusted model Address and changed the home route.
    Added fillable field to model Address and changed home route to go to root.

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'city_id',
+        'region',
+        'street_name',
+        'postal_code',
+        'comments'
+    ];
 }
